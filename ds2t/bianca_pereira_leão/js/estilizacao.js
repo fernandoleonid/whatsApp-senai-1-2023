ce420ec5278d2fc62178680f2fa17da4829cb123
unknown <biancapeleao@gmail.com>
2023-02-24 09:43:10 -0300

    estilizando padding do input para ux

--- ds2t/bianca_pereira_leão/js/estilizacao.js
+++ ds2t/bianca_pereira_leão/js/estilizacao.js
@@ -5,11 +5,18 @@ const searchIcon = document.querySelector('#search');
 const arrowIcon = document.querySelector('#arrow');
 
 searchInput.addEventListener('focus', function () {
+    const iconWidth = searchIcon.offsetWidth;
+    const paddingLeft = iconWidth + 40;
+    searchInput.style.paddingLeft = `${paddingLeft}px`;
     searchIcon.style.display = 'none';
     arrowIcon.style.display = 'inline-block';
 });
 
 searchInput.addEventListener('blur', function () {
+
+    const iconWidth = searchIcon.offsetWidth;
+    const paddingLeft = iconWidth + 0;
+    searchInput.style.paddingLeft = `${paddingLeft}px`;
     searchIcon.style.display = 'inline-block';
     arrowIcon.style.display = 'none';
 });

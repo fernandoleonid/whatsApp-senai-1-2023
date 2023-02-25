0e6a7f22226e74c692177f16bc521ae59ba5e033
nicanico <nicsousan@gmail.com>
2023-02-24 23:16:29 -0300

    update 24/02 - inicialização da base

---
+++ ds2t/nicole_souza_santos/script.js
@@ -0,0 +1,9 @@
+'use strict'
+
+
+
+// carregue e exiba contatos primeiro
+// exibir conversa
+// troca de tema
+// comece trabalhar na versão mobile seguindo essa mesma lógica
+// vc consegue (vc precisa pq não tem saida :))

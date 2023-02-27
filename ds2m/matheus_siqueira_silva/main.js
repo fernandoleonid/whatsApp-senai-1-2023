f644840cb614ac5010b0c5a22be054b9670551d9
Ma7hs <Matheus.siqueira100@gmail.com>
2023-02-27 10:54:34 -0300

    commiting

--- ds2m/matheus_siqueira_silva/main.js
+++ ds2m/matheus_siqueira_silva/main.js
@@ -1,17 +1,40 @@
+'use strict'
+
 import {contatos} from "./recursos/contatos.js";
 
 
 const createChat = (contato) => {
-    
+
+
+    const chat = document.createElement('div')
+    chat.classList.add('user__talked')
+
     const img = document.createElement('img') 
     img.classList.add('user__image')
-    img.src = `./recursos/images/${contatos.}`
+    img.src = `./recursos/${contato.image}`
+
+    const text__user = document.createElement('div')
+    text__user.classList.add('text__user')
+
+    const nome = document.createElement('h5')
+    nome.classList.add('user__name')
+    nome.textContent = contato.name
+    
+    const description = document.createElement('span')
+    description.classList.add('user__conversation')
+    description.textContent = contato.description
+
+    
+    text__user.append(nome, description)
+    chat.append(img, text__user)
+
+    return chat
 }
 
 const loadingContact = () => {
-    const users = document.getElementById('users')
-    const informations = contatos.map();
-
+   
+    const users = document.getElementById('container__chat')
+    const informations = contatos.map(createChat);
     users.replaceChildren(...informations)
 }
 

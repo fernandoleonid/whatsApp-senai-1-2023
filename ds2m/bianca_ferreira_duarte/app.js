608adf5b3238729fd62154ea275c551b891a906a
Bianca Duarte <123369796+biiaduartez@users.noreply.github.com>
2023-03-01 20:18:13 -0300

    framework Pure.css adicionado

---
+++ ds2m/bianca_ferreira_duarte/app.js
@@ -0,0 +1,44 @@
+'use strict'
+
+import { contatos } from "./contatos.js"
+
+const criarContainer = (contato) => {
+    const container = document.createElement('div')
+    container.classList.add('contact__container')
+
+    const imageHolder = document.createElement('div')
+    imageHolder.classList.add('nav__image_holder')
+
+    const image = document.createElement('img')
+    image.classList.add('nav__main_image')
+    image.src = `./recursos/${contato.image}`
+    imageHolder.append(image)
+
+    const contact = document.createElement('div')
+    contact.classList.add('contact__text')
+
+    const name = document.createElement('h5')
+    name.classList.add('contact__name')
+    name.textContent = contato.name
+    contact.append(name)
+
+    const description = document.createElement('p')
+    description.classList.add('contact__last__menssage')
+    description.textContent = contato.description
+    contact.append(description)
+
+    container.append(imageHolder, contact)
+
+    return container
+
+
+}
+
+const carregarMensagens = () => {
+    const navMain = document.getElementById('nav__main')
+    const cards = contatos.map(criarContainer)
+
+    navMain.replaceChildren(...cards)
+}
+
+carregarMensagens()

ee6ba9d9d41b5d892324674372324f6b0f6d1958
Yasmin Gonçalves <yasmin.coelho4@senaisp.edu.br>
2023-03-02 16:03:24 -0300

    Aba de contatos finalizada

--- ds2t/yasmin_goncalves_coelho/index.js
+++ ds2t/yasmin_goncalves_coelho/index.js
@@ -2,25 +2,37 @@
 
 import { contatos } from "./contatos.js"
 
-
-const criarContato = (contato) => {
+const criarContato = (contact) => {
     const contato = document.createElement('div')
     contato.classList.add('contact')
 
     const foto = document.createElement('img')
     foto.classList.add('contact__picture')
-    foto.src = `./images/${contato.image}`
+    foto.src = contact.image
 
-    contato.append(foto)
+    const textos = document.createElement('span')
+    textos.classList.add('contact__text')
+
+    const nome = document.createElement('h3')
+    nome.classList.add('contact__name')
+    nome.textContent = contact.name
+
+    const descricao = document.createElement('p')
+    descricao.classList.add('contact__description')
+    descricao.textContent = contact.description
+
+    textos.append(nome, descricao)
+
+    contato.append(foto, textos)
 
     return contato
 }
 
 const carregarContatos = () => {
-    const container = document.getElementById('container')
-    const contatos = contatos.map(criarContato)
+    const container = document.getElementById('contacts__container')
+    const contacts = contatos.map(criarContato)
 
-    container.replaceChild(contatos)
+    container.replaceChildren(...contacts)
 }
 
 carregarContatos()

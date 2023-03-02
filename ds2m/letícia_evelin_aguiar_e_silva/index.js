64bac0b31d452278f8c984659b67dcaa4153b715
LETÍCIA EVELIN AGUIAR E SILVA <22282118@CFP127ALUNO.LOCAL>
2023-03-02 11:02:04 -0300

    div mensagens iniciado

--- ds2m/letícia_evelin_aguiar_e_silva/index.js
+++ ds2m/letícia_evelin_aguiar_e_silva/index.js
@@ -4,37 +4,45 @@
  import { contatos } from "./contatos.js"
 
 
- const criarCard = (contatos) =>{
+ const criarCard = (contato) =>{
     const card = document.createElement( 'div' )
     card.classList.add( 'card' )
 
     const img = document.createElement( 'img' )
     img.classList.add( 'card__image' )
-    img.src = `./img/${contatos.image}`
-
-    const titulo_texto = document.createElement('div')
-    titulo_texto.classList.add('titulo_texto')
+    img.src = `./img/${contato.image}`
 
     const titulo = document.createElement( 'h5' )
     titulo.classList.add( 'card__titulo' )
-    titulo.textContent = contatos.name
+    titulo.textContent = contato.name
+
+    const texto = document.createElement('div')
+    texto.classList.add('titulo_texto')
 
     const profissao = document.createElement( 'p' )
     profissao.classList.add( 'card__profissao' )
-    profissao.textContent = contatos.description
+    profissao.textContent = contato.description
 
-    titulo_texto.append(titulo,profissao)
-    card.append(img, titulo_texto)
+   //  titulo_texto.append(titulo,profissao)
+    card.append(img, titulo, profissao, texto)
 
     return card
 
  }
 
+//  const addNomeContato = (nome) => {
+//    const titulo = document.createElement('h3')
+//    titulo.classList.add('titulo__nome')
+//    titulo.textContent = nome.name
+//  }
+
  const carregarApp = () => {
 
     const container = document.getElementById( 'container' )
     const cards = contatos.map( criarCard )
+   //  const mensagens = document.getElementById( 'mensagens' )
 
+   //const 
     container.append(...cards)
  }
 

f65bf04bd01c63cfecec0d093ba72a222bae2080
Dan Lino <danielalinodasilvaoliveira@gmail.com>
2023-03-02 02:27:14 -0300

    progresso na tentativa de expor chat - 02/02/2023

--- ds2t/daniela_lino_da_silva_oliveira/script.js
+++ ds2t/daniela_lino_da_silva_oliveira/script.js
@@ -1,10 +1,12 @@
 'use strict'
 import { contatos } from "../../recursos/contatos.js";
-console.log(contatos)
 
 const criarContato = (contato) => {
+
+
     const cardContact = document.createElement('div')
     cardContact.classList.add('contact')
+    cardContact.setAttribute('id', 'contact')
 
     const foto = document.createElement('img')
     foto.classList.add('contact__image')
@@ -21,7 +23,7 @@ const criarContato = (contato) => {
     contactDescription.classList.add('contact__description')
     contactDescription.textContent = contato.description
 
-    cardTitle.append(nameContact, contactDescription)
+    cardTitle.append(nameContact, contactDescription);
     cardContact.append(foto, cardTitle);
 
     return cardContact;
@@ -31,6 +33,69 @@ const carregarContatos = () => {
     const cardsContato = contatos.map(criarContato);
 
     containerContatos.replaceChildren(...cardsContato);
-    console.log('ok')
+}
+const criarChat = (json) => {
+    const nomeContato = json.name
+    const senderMessage = json.messages
+    // const nomeContato = json.name
+    // const senderMessage = json.messages.sender
+    console.log(senderMessage.lenght)
+
+
+
+    for (let cont = 0; cont < senderMessage.length; cont++) {
+        const cardChat = document.createElement('div');
+        cardChat.classList.add('chat_place');
+        console.log(3)
+        console.log(senderMessage[1].sender)
+        if (senderMessage[cont].sender != nomeContato) {
+            console.log('ok')
+            const messageRight = document.createElement('div')
+            messageRight.classList.add('right__messages')
+
+            const userMessage = document.createElement('div')
+            userMessage.classList.add('user__message')
+
+            const messageContentRight = document.createElement('p')
+            messageContentRight.classList.add('message')
+            messageContentRight.textContent = senderMessage[cont].content
+
+            messageRight.append(userMessage, messageContentRight);
+            cardChat.append(messageRight);
+            return cardChat;
+        } else {
+            const messageLeft = document.createElement('div')
+            messageLeft.classList.add('left__messages')
+
+            const contactMessage = document.createElement('div')
+            contactMessage.classList.add('contact__message')
+
+            const messageContentLeft = document.createElement('p')
+            messageContentLeft.classList.add('message')
+            messageContentLeft.textContent = senderMessage[cont].content
+
+            messageLeft.append(contactMessage, messageContentLeft);
+
+            cardChat.append(messageLeft);
+            return cardChat;
+        }
+    }
+
+
+}
+
+
+
+
+const carregarChat = () => {
+    const containerChat = document.getElementById('messages_place')
+    const cardsChat = contatos.map(criarChat);
+
+    containerChat.replaceChildren(...cardsChat);
+}
+const contactClick = () => {
+    const contatoExistente = document.getElementById('contact');
+    contatoExistente.addEventListener('click', carregarChat)
 }
 carregarContatos()
+contactClick()

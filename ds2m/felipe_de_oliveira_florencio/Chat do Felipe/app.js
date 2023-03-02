af70f3673857633cdf2307a19ce67446704d5a5d
FELIPE DE OLIVEIRA FLORENCIO <22282108@CFP127ALUNO.LOCAL>
2023-03-02 10:06:23 -0300

    Criado construção de contatos dinâmica com javascript

--- ds2m/felipe_de_oliveira_florencio/Chat do Felipe/app.js
+++ ds2m/felipe_de_oliveira_florencio/Chat do Felipe/app.js
@@ -1,36 +1,61 @@
-import {contatos} from "./resources/contatos.js"
-
 'use strict'
 
+import {contatos} from "./resources/api/contatos.js"
 
-const homeContainer = document.getElementById('home')
+
 const buildHome = function(){
-    
+    const contactContainer = document.getElementById('contact')
+    const contacts = contatos.map(buildContact)
+    contactContainer.append(...contacts)
 }
 
-const buildContatos = (contato) => {
-    const card = document.createElement('div')
-    card.classList.add('card')
+const buildContact = (contact) => {
+    const contactDiv = document.createElement('div')
+    contactDiv.classList.add('contact')
 
-    const name = document.createElement('h5')
-    titulo.classList.add('card__title')
-    titulo.textContent = contato.name
+    const info = document.createElement('div')
+    info.classList.add('contact__info')
 
-    const img = document.createElement('img')
-    img.classList.add('card__image')
-    img.src = `./img/${contato.image}`
+    const name = document.createElement('h2')
+    name.classList.add('contact__name')
+    name.textContent = contact.name
 
-    
+    const image = document.createElement('img')
+    image.classList.add('contact__photo')
+    image.src = `./resources/api/${contact.image}`
 
-    const descricao = document.createElement('p')
-    descricao.classList.add('card__description')
-    descricao.textContent = contato.description
+    const description = document.createElement('p')
+    description.classList.add('contact__description')
+    description.textContent = contact.description
 
-    const price = document.createElement('p')
-    price.classList.add('card__price')
-    price.textContent = contato.price
-    card.append(img,titulo,descricao,price)
+    const button = document.createElement('button')
+    button.classList.add('contact__button')
 
-    return card
+    const vetor = document.createElement('img')
+    vetor.src = `./resources/imgs/vetor.png`
+
+    const text = document.createElement('p')
+    text.textContent = 'Ver'
+
+    // const messages = document.createElement('p')
+    // price.classList.add('card__price')
+    // price.textContent = contato.price
+
+    info.append(
+        name,
+        description
+    )
+    button.append(
+        vetor,
+        text
+    )
+    contactDiv.append(
+        image,
+        info,
+        button
+    )
+
+    return contactDiv
 
 }
+buildHome()

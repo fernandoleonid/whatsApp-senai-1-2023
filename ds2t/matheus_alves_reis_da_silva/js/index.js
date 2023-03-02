2bea8767e2c93288c3d3878bcff24cb60cffd6fe
Matheus Alves <mts02501@gmail.com>
2023-03-02 16:44:06 -0300

    faltando apenas as mensagens

--- ds2t/matheus_alves_reis_da_silva/js/index.js
+++ ds2t/matheus_alves_reis_da_silva/js/index.js
@@ -1,27 +1,58 @@
 'use strict'
 
-import {contatos} from "../js/contatos.js"
+import {contatos} from "./contatos.js"
 
 const criaContato = (contato) => {
-    const active = document.createElement('div')
-    active.classList.add('active')
+    const messages_chat = document.createElement('ul')
+    messages_chat.classList.add('chat_list')
 
-    const foto = document.createElement('img')
-    foto.classList.add('profile')
-    foto.src = `../img/${contato.image}`
+    const chat = document.createElement('div')
+    chat.classList.add('chat_active')
 
-    const nome = document.createElement('h4')
-    nome.classList.add('listHead')
-    nome.textContent = contato.name
+    const photo = document.createElement('img')
+    photo.classList.add('img_contact')
+    photo.src = `./${contato.image}`
 
-    contato.append(foto, nome)
+    const details = document.createElement('div')
+    details.classList.add('chat_details')
+
+    const name = document.createElement('span')
+    name.textContent = contato.name
+
+    const message = document.createElement('span')
+    message.classList.add('message_contact')
+    message.textContent = contato.description
+
+    messages_chat.append(chat)
+
+    chat.append(photo, details, name)
+
+    details.append(name, message)
+
+    return messages_chat
 }
 
 const carregarContatos = () => {
-    const chatList = document.getElementById('chatList')
-    const contacts = contatos.map(criaContato)
+    const chat_messages = document.getElementById('chat_messages')
+    const contactsMessages = contatos.map(criaContato)
 
-    chatList.replaceChildren(...contacts)
+    chat_messages.replaceChildren(...contactsMessages)
 }
 
+const adicionarMensagem = (sender, content, time) => {
+    const remetente = remetente.textContent(contato.sender)
+    const conteudo = content.textContent(contato.content)
+    const hora = content.textContent(contato.time)
+
+}    
+
+const handleClick = () => {
+    const sender = prompt ('me')
+    const content = prompt ('Hello Leonid')
+    const time = prompt ('22:20')
+
+    adicionarMensagem(sender, content, time)
+}
+
+addEventListener('click', handleClick)
 carregarContatos()

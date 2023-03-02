dd75f0f9971b712ec546d8e2342aac191e552c9d
VINICIUS NUNES DE OLIVEIRA <22282114@CFP127ALUNO.LOCAL>
2023-03-02 11:24:07 -0300

    semi-finalizado

--- ds2m/vinicius_nunes_de_oliveira/app.js
+++ ds2m/vinicius_nunes_de_oliveira/app.js
@@ -3,40 +3,101 @@
 import { contatos } from './recursos/contatos.js'
 
 //media__contacts
+let i = 0
+
 
 const criarContato = contato => {
-  const card = document.createElement('div')
-  card.classList.add('media__contacts')
 
-  const img = document.createElement('img')
-  img.classList.add('contact-img')
-  img.src = `./${contato.image}`
-  // img.src = '../img/' + contatos.image
+    const card = document.createElement('button')
+    card.classList.add('media__contacts')
 
-  const messages = document.createElement('div')
-  messages.classList.add('messages')
 
-  const nome = document.createElement('h5')
-  nome.classList.add('messages__name')
-  nome.textContent = contato.name
+    const img = document.createElement('img')
+    img.classList.add('contact-img')
+    img.src = `./${contato.image}`
+        // img.src = '../img/' + contatos.image
 
-  const descricao = document.createElement('span')
-  descricao.classList.add('messages__description')
-  descricao.textContent = contato.messages[0].content
+    const messages = document.createElement('div')
+    messages.classList.add('messages')
 
-  // img.classList.add('card__image')
-  // img.src = `./${contatos.image}`
-  messages.append(nome, descricao)
-  card.append(img, messages)
+    const nome = document.createElement('h5')
+    nome.classList.add('messages__name')
+    nome.textContent = contato.name
+    nome.id = 'card'
 
-  return card
+    const descricao = document.createElement('span')
+    descricao.classList.add('messages__description')
+        // descricao.textContent = contato.description
+    descricao.textContent = contato.messages[2].content
+
+    // img.classList.add('card__image')
+    // img.src = `./${contatos.image}`
+    messages.append(nome, descricao)
+    card.append(img, messages)
+    card.id = i++
+
+
+        return card
 }
 
 const carregarContatos = () => {
-  const container = document.getElementById('container')
-  const cards = contatos.map(criarContato)
+    const container = document.getElementById('container')
+    const cards = contatos.map(criarContato)
 
-  container.replaceChildren(...cards)
+    container.replaceChildren(...cards)
+
+    cards.forEach(card => {
+        card.onclick = () => {
+            // let alo = document.querySelectorAll('#card2')
+            // let alo = document.getElementById(card2.id)
+            let cardIdd = document.getElementById(card)
+            console.log(contatos[card.id].messages)
+
+        }
+    });
 }
 
+
+
 carregarContatos()
+
+
+const cardHtml = document.getElementById('1')
+
+// cardHtml.onclick = (card1111) => {
+//     alert(card1111.nome)
+// }
+
+
+
+
+
+
+
+// let cardSelecionado = getElementById('card')
+
+// const teste = document.getElementById('card')
+
+// teste.onclick = () => {
+//     const teste2 = document.getElementById('card')
+//     alert(teste2.textContent)
+// }
+
+// const cardHtml = document.getElementById("1")
+
+// forEach(function(cardHtml) {
+//     card.addEventListener("click", handleClick)
+
+//     const handleClick = (card) => {
+
+//         alert(card)
+//         return true
+
+//     }
+// })
+
+
+// // const button = document.querySelector("#card");
+// // button.addEventListener("click", (e) => {
+// //     alert(e.nome); // my-btn
+// // })

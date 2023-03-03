f264dedecca789d2effa92ac4bb95807f212aa80
VINICIUSNUNES137 <90266473+VINICIUSNUNES137@users.noreply.github.com>
2023-03-02 23:13:34 -0300

    inside message in progress

--- ds2m/vinicius_nunes_de_oliveira/app.js
+++ ds2m/vinicius_nunes_de_oliveira/app.js
@@ -5,74 +5,98 @@ import { contatos } from './recursos/contatos.js'
 //media__contacts
 let i = 0
 
-
 const criarContato = contato => {
+  const card = document.createElement('button')
+  card.classList.add('media__contacts')
 
-    const card = document.createElement('button')
-    card.classList.add('media__contacts')
+  const img = document.createElement('img')
+  img.classList.add('contact-img')
+  img.src = `./${contato.image}`
+  // img.src = '../img/' + contatos.image
 
+  const messages = document.createElement('div')
+  messages.classList.add('messages')
 
-    const img = document.createElement('img')
-    img.classList.add('contact-img')
-    img.src = `./${contato.image}`
-        // img.src = '../img/' + contatos.image
+  const nome = document.createElement('h5')
+  nome.classList.add('messages__name')
+  nome.textContent = contato.name
+  nome.id = 'card'
 
-    const messages = document.createElement('div')
-    messages.classList.add('messages')
+  const descricao = document.createElement('span')
+  descricao.classList.add('messages__description')
+  // descricao.textContent = contato.description
+  descricao.textContent = contato.messages[2].content
 
-    const nome = document.createElement('h5')
-    nome.classList.add('messages__name')
-    nome.textContent = contato.name
-    nome.id = 'card'
+  messages.append(nome, descricao)
+  card.append(img, messages)
+  card.id = i++
 
-    const descricao = document.createElement('span')
-    descricao.classList.add('messages__description')
-        // descricao.textContent = contato.description
-    descricao.textContent = contato.messages[2].content
-
-    // img.classList.add('card__image')
-    // img.src = `./${contatos.image}`
-    messages.append(nome, descricao)
-    card.append(img, messages)
-    card.id = i++
-
-
-        return card
+  return card
 }
 
 const carregarContatos = () => {
-    const container = document.getElementById('container')
-    const cards = contatos.map(criarContato)
+  const container = document.getElementById('container')
+  const cards = contatos.map(criarContato)
 
-    container.replaceChildren(...cards)
+  container.replaceChildren(...cards)
 
-    cards.forEach(card => {
-        card.onclick = () => {
-            // let alo = document.querySelectorAll('#card2')
-            // let alo = document.getElementById(card2.id)
-            let cardIdd = document.getElementById(card)
-            console.log(contatos[card.id].messages)
+  cards.forEach(card => {
+    card.onclick = () => {
+      // let alo = document.querySelectorAll('#card2')
+      // let alo = document.getElementById(card2.id)
+      //   let cardIdd = document.getElementById(card)
+      console.log(contatos[card.id].image)
 
-        }
-    });
+      const cardContainer2 = document.createElement('div')
+      cardContainer2.classList.add('message__header')
+
+      const cardContainer = document.createElement('div')
+      cardContainer.classList.add('group')
+
+      const imgCard = document.createElement('img')
+      imgCard.classList.add('contact-img')
+      imgCard.src = `./${contatos[card.id].image}`
+
+      const header__contact = document.createElement('div')
+      header__contact.classList.add('header__contact')
+
+      const header__name = document.createElement('h5')
+      header__name.classList.add('messages__name')
+      header__name.textContent = contatos[card.id].name
+
+      const messages__description = document.createElement('span')
+      messages__description.classList.add('messages__description')
+      messages__description.textContent = contatos[card.id].description
+
+      //   const conjIcon = document.createElement('div')
+      //   conjIcon.classList.add('header__icon')
+
+      //   const iconClip = document.createElement('i')
+      //   iconClip.classList.add('fas fa-ellipsis-v')
+
+      //   const iconDot = document.createElement('i')
+      //   iconDot.classList.add('fas fa-paperclip')
+
+      //   conjIcon.append(iconClip, iconDot)
+
+      header__contact.append(header__name, messages__description)
+
+      cardContainer.append(imgCard, header__contact)
+      cardContainer2.append(cardContainer)
+      //   cardContainer2.append(cardContainer,conjIcon)
+
+      const containerMessage = document.getElementById('inside-message')
+
+      containerMessage.replaceChildren(cardContainer2)
+    }
+  })
 }
 
-
-
 carregarContatos()
-
-
-const cardHtml = document.getElementById('1')
 
 // cardHtml.onclick = (card1111) => {
 //     alert(card1111.nome)
 // }
-
-
-
-
-
-
 
 // let cardSelecionado = getElementById('card')
 
@@ -95,7 +119,6 @@ const cardHtml = document.getElementById('1')
 
 //     }
 // })
-
 
 // // const button = document.querySelector("#card");
 // // button.addEventListener("click", (e) => {

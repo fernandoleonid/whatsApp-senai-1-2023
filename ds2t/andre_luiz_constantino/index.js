c16eff8835abe29c6f241110206cd9ef39b734c0
André Luiz Constantino <andreluis081205@gmail.com>
2023-03-04 16:03:30 -0300

    Header sendo criado com JSON feito

--- ds2t/andre_luiz_constantino/index.js
+++ ds2t/andre_luiz_constantino/index.js
@@ -2,15 +2,19 @@
 
 import { contatos } from "./recursos/contatos.js"
 
-
-const criarContato = ( contato ) => {
+const criarContato = (contato, indice) => {
     const contact = document.createElement('div')
     contact.classList.add('contact-card__container')
+    contact.setAttribute('id', 'id-' + indice)
+
+    contact.addEventListener('click', (event) => {
+        carregarConversas(indice)
+        console.log(contatos[indice])
+    })
 
     const fotoDePerfil = document.createElement('img')
     fotoDePerfil.classList.add('contact-card__image')
     fotoDePerfil.src = `./${contato.image}`
-
 
     const nomeComDescricao = document.createElement('div')
     nomeComDescricao.classList.add('contact-card__name-description')
@@ -31,9 +35,40 @@ const criarContato = ( contato ) => {
 
 const carregarContato = () => {
     const containerContatos = document.getElementById('contacts-container')
-    const contato = contatos.map( criarContato )
+    const contato = contatos.map(criarContato)
 
     containerContatos.replaceChildren(...contato)
+}
+
+const carregarConversas = (indice) => {
+    const container = document.getElementById('container-messages')
+    const header = document.createElement('div')
+    header.classList.add('contact-information__container')
+
+    const imagemDePerfil = document.createElement('img')
+    imagemDePerfil.classList.add('contact-information__image')
+    imagemDePerfil.src = `./${contatos[indice].image}`
+
+    const juntarImagemComTextos = document.createElement('div')
+    juntarImagemComTextos.classList.add('contact-information__image-name-description')
+
+    const juntarTextos = document.createElement('div')
+    juntarTextos.classList.add('contact-information__name-description')
+
+    const nomeDoPerfil = document.createElement('p')
+    nomeDoPerfil.classList.add('contact-information__name')
+    nomeDoPerfil.textContent = contatos[indice].name
+
+    const descricaoDoPerfil = document.createElement('p')
+    descricaoDoPerfil.classList.add('contact-information__description')
+    descricaoDoPerfil.textContent = contatos[indice].description
+
+    juntarTextos.append(nomeDoPerfil, descricaoDoPerfil)
+    juntarImagemComTextos.append(imagemDePerfil, juntarTextos)
+    header.append(juntarImagemComTextos)
+
+
+    container.append(header)
 }
 
 carregarContato()

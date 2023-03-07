a405632b290ac9adea16539217a69811a7800694
VINICIUSNUNES137 <90266473+VINICIUSNUNES137@users.noreply.github.com>
2023-03-06 23:43:13 -0300

    puxando mensagens

--- ds2m/vinicius_nunes_de_oliveira/app.js
+++ ds2m/vinicius_nunes_de_oliveira/app.js
@@ -25,6 +25,8 @@ const criarContato = contato => {
   const descricao = document.createElement('span')
   descricao.classList.add('messages__description')
   // descricao.textContent = contato.description
+  let contador = 0
+
   descricao.textContent = contato.messages[2].content
 
   messages.append(nome, descricao)
@@ -45,8 +47,7 @@ const carregarContatos = () => {
       // let alo = document.querySelectorAll('#card2')
       // let alo = document.getElementById(card2.id)
       //   let cardIdd = document.getElementById(card)
-      console.log(contatos[card.id].image)
-
+      // console.log(contatos[card.id].image)
       const cardContainer2 = document.createElement('div')
       cardContainer2.classList.add('message__header')
 
@@ -68,59 +69,87 @@ const carregarContatos = () => {
       messages__description.classList.add('messages__description')
       messages__description.textContent = contatos[card.id].description
 
-      //   const conjIcon = document.createElement('div')
-      //   conjIcon.classList.add('header__icon')
-
-      //   const iconClip = document.createElement('i')
-      //   iconClip.classList.add('fas fa-ellipsis-v')
-
-      //   const iconDot = document.createElement('i')
-      //   iconDot.classList.add('fas fa-paperclip')
-
-      //   conjIcon.append(iconClip, iconDot)
-
       header__contact.append(header__name, messages__description)
 
       cardContainer.append(imgCard, header__contact)
       cardContainer2.append(cardContainer)
       //   cardContainer2.append(cardContainer,conjIcon)
+      const contentMessage = document.createElement('div')
+      contentMessage.classList.add('message__content')
+
+      const cardDaMensagem = document.createElement('div')
+      cardDaMensagem.classList.add('message__content__card')
+      //começa aquiiiiii
+      contatos[card.id].messages.forEach(function (mensagem) {
+        if (mensagem.sender == 'me') {
+          const meContent = document.createElement('div')
+          meContent.classList.add('content__me')
+
+          const alignMe = document.createElement('div')
+          alignMe.classList.add('me__align')
+
+          const sendMe = document.createElement('h5')
+          sendMe.classList.add('me__send')
+          sendMe.textContent = mensagem.sender
+
+          const messageMe = document.createElement('h5')
+          messageMe.classList.add('me__message')
+          messageMe.textContent = mensagem.content
+
+          const dateMe = document.createElement('span')
+          dateMe.classList.add('me__date')
+          dateMe.textContent = mensagem.time
+
+          alignMe.append(sendMe, messageMe, dateMe)
+          cardDaMensagem.append(alignMe)
+        } else {
+          const otherContent = document.createElement('div')
+          otherContent.classList.add('content__other')
+
+          const alignOther = document.createElement('div')
+          alignOther.classList.add('other__align')
+
+          const sendOther = document.createElement('h5')
+          sendOther.classList.add('other__send')
+          sendOther.textContent = mensagem.sender
+
+          const messageOther = document.createElement('h5')
+          messageOther.classList.add('other__message')
+          messageOther.textContent = mensagem.content
+
+          const dateOther = document.createElement('span')
+          dateOther.classList.add('other__date')
+          dateOther.textContent = mensagem.time
+
+          alignOther.append(sendOther, messageOther, dateOther)
+          cardDaMensagem.append(alignOther)
+        }
+        contentMessage.appendChild(cardDaMensagem)
+      })
 
       const containerMessage = document.getElementById('inside-message')
 
-      containerMessage.replaceChildren(cardContainer2)
+      containerMessage.replaceChildren(cardContainer2, contentMessage)
+
+      //criando mensagens
+
+      /* 
+       <div class="message__content">
+          <div class="content__me">
+            <div class="me__align">
+              <h5 class="me__send">me</h5>  //  /////
+              <span class="me__message" />//////
+                >Hello Leonid, how are you doing today?Hello Leonid, how are you
+                doing today?Hello Leonid, how are you doing today?Hello Leonid,
+                how are you doing today?Hello Leonid, how are you doing
+                today?Hello Leonid, how are you doing today?Hello Leonid, how
+                are you doing today?</span
+              >
+              <span class="me__date">14:25</span>
+            </div>
+          </div>*/
     }
   })
 }
 
 carregarContatos()
-
-// cardHtml.onclick = (card1111) => {
-//     alert(card1111.nome)
-// }
-
-// let cardSelecionado = getElementById('card')
-
-// const teste = document.getElementById('card')
-
-// teste.onclick = () => {
-//     const teste2 = document.getElementById('card')
-//     alert(teste2.textContent)
-// }
-
-// const cardHtml = document.getElementById("1")
-
-// forEach(function(cardHtml) {
-//     card.addEventListener("click", handleClick)
-
-//     const handleClick = (card) => {
-
-//         alert(card)
-//         return true
-
-//     }
-// })
-
-// // const button = document.querySelector("#card");
-// // button.addEventListener("click", (e) => {
-// //     alert(e.nome); // my-btn
-// // })

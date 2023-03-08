f4f4973e27ab09b025ffc0b6aa1608018fa40261
ANDRE LUIZ CONSTANTINO <00001058445522sp@aluno.educacao.sp.gov.br>
2023-03-08 09:20:39 -0300

    Mensagens com evento

--- ds2t/andre_luiz_constantino/index.js
+++ ds2t/andre_luiz_constantino/index.js
@@ -9,7 +9,6 @@ const criarContato = (contato, indice) => {
 
     contact.addEventListener('click', (event) => {
         carregarConversas(indice)
-        console.log(contatos[indice])
     })
 
     const fotoDePerfil = document.createElement('img')
@@ -41,6 +40,10 @@ const carregarContato = () => {
 }
 
 const carregarConversas = (indice) => {
+    carregarMensagensComHeader(indice)
+}
+
+const carregarMensagensComHeader = (indice) => {
     const container = document.getElementById('container-messages')
     const header = document.createElement('div')
     header.classList.add('contact-information__container')
@@ -63,12 +66,45 @@ const carregarConversas = (indice) => {
     descricaoDoPerfil.classList.add('contact-information__description')
     descricaoDoPerfil.textContent = contatos[indice].description
 
+    const containerMessages = document.getElementById('container-messages')
+    const messagesContainer = document.createElement('div')
+    container.classList.add('container-messages')
+    messagesContainer.classList.add('messages__container')
+    
     juntarTextos.append(nomeDoPerfil, descricaoDoPerfil)
     juntarImagemComTextos.append(imagemDePerfil, juntarTextos)
     header.append(juntarImagemComTextos)
 
-
     container.append(header)
+
+    contatos[indice].messages.forEach(function (contato) {
+        //Processo para criar uma div
+        const divMensagem = document.createElement('div')
+        divMensagem.classList.add('message-text-contact__container')
+
+        const textMensagem = document.createElement('p')
+        textMensagem.textContent = 'texto'
+        
+        const textHorario = document.createElement('span')
+        textHorario.textContent = '14:20'
+
+        const divMensagem2 = document.createElement('div')
+        divMensagem2.classList.add('message-text__container')
+
+        const textMensagem2 = document.createElement('p')
+        textMensagem2.textContent = 'texto'
+
+        const textHorario2 = document.createElement('span')
+        textHorario2.textContent = '14:20'
+
+        
+        divMensagem.append(textMensagem, textHorario)
+        divMensagem2.append(textMensagem2, textHorario2)
+        messagesContainer.append(divMensagem, divMensagem2)
+        containerMessages.append(messagesContainer)
+    })
+
+    
 }
 
 carregarContato()

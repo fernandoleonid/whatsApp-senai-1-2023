aa88260806dd16e5d54e3601b95ffdd10d32b0a1
Leticia Evelin <ljkeleticia@gmail.com>
2023-03-08 19:08:55 -0300

    layout, inicio js conversas

--- ds2m/letícia_evelin_aguiar_e_silva/index.js
+++ ds2m/letícia_evelin_aguiar_e_silva/index.js
@@ -40,10 +40,38 @@
 
     const container = document.getElementById( 'container' )
     const cards = contatos.map( criarCard )
-   //  const mensagens = document.getElementById( 'mensagens' )
 
    //const 
     container.append(...cards)
  }
 
  carregarApp()
+
+ const getMensagens = (mensagem) => {
+
+   const divMensagem = document.createElement('div')
+
+   let envioConversa = document.createElement('span')
+
+   if(mensagem.sender == "me") {
+      divMensagem.classList.add('enviar__mensagem')
+      envioConversa.classList.add('enviar__mensagem_span')
+      envioConversa.textContent = mensagem.content
+   } else {
+      divMensagem.classList.add('mensagem__entregue')
+      envioConversa.classList.add('mensagem__entregue_span')
+      envioConversa.textContent = mensagem.content
+   }
+
+   divMensagem.append(envioConversa)
+   return divMensagem
+
+ }
+
+ const carregarMensagens = (indice) => {
+   const mensagemContato = document.getElementById('mensagem__contato')
+   const mensagem = contatos[indice].mensagem.map(getMensagens)
+
+   mensagemContato.replaceChildren(...mensagem)
+
+}

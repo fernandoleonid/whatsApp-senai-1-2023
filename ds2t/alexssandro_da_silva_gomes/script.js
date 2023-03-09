47de10e97b7d3151e9c76e8321e829929510296e
Alexssandro <22282169@CFP127ALUNO.LOCAL>
2023-03-09 16:52:49 -0300

    elemento usuário em processo

--- ds2t/alexssandro_da_silva_gomes/script.js
+++ ds2t/alexssandro_da_silva_gomes/script.js
@@ -1,1 +1,42 @@
 'use strict'
+
+import { contatos } from "./contatos.js"
+
+const criarContato = (contato) => {
+
+    const contact = document.createElement('div')
+    contact.classList.add("aside__main--contact")
+
+    const pic = document.createElement('div')
+    pic.classList.add("aside__main--contact-pic")
+
+    const image = document.createElement('img')
+    image.classList.add('image')
+    image.src = `./img${contato.image}`
+
+    const content = document.createElement('div')
+    content.classList.add("aside__main--contact-content")
+
+    const name = document.createElement('h4')
+    name.textContent = contato.name
+
+    const description = document.createElement('p')
+    description.textContent = contato.description
+
+    contact.append(pic, content)
+    pic.append(image)
+    content.append(name, description)
+
+    return contact
+}
+
+
+
+const carregarContatos = () => {
+    const container = document.getElementById('container')
+    const contato = contatos.map(criarContato)
+
+    container.replaceChildren(...contato)
+}
+
+carregarContatos()

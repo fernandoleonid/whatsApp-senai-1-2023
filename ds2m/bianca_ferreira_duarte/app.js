8ebd75c6aef39fcd58f3f14858b646bfe1be4c64
BIANCA FERREIRA DUARTE <22282117@CFP127ALUNO.LOCAL>
2023-03-16 07:51:11 -0300

    modificações de cores

--- ds2m/bianca_ferreira_duarte/app.js
+++ ds2m/bianca_ferreira_duarte/app.js
@@ -1,22 +1,23 @@
-`use strict`
+'use strict'
 
-import {contatos} from "./recursos/contatos.js"
+import { contatos } from "./recursos/contatos.js"
 var id = 0
 
 const criarContainer = (contato) => {
-    document.getElementById("send").addEventListener("click",addBallon)
+    document.getElementById("send").addEventListener("click", addBallon)
     document.getElementById("send__textfield").addEventListener("keypress", (key) => {
-        if(key.key === 'Enter'){
-             addBallon()
-        }})
+        if (key.key === 'Enter') {
+            addBallon()
+        }
+    })
 
     const container = document.createElement('div')
     container.classList.add('contact__container')
     container.style.backgroundColor
-    container.setAttribute('id',id)
+    container.setAttribute('id', id)
     id++
     container.addEventListener("click", mudarFundo)
-    container.addEventListener("click",criarMensagens)
+    container.addEventListener("click", criarMensagens)
 
     const imageHolder = document.createElement('div')
     imageHolder.classList.add('nav__image_holder')
@@ -54,27 +55,25 @@ const criarBarras = () => {
 }
 
 const mudarTela = () => {
-    if(window.innerWidth < 770){
+    if (window.innerWidth < 770) {
         document.getElementById("nav").style.display = "none"
-        document.getElementById("main").style.display = "flex" 
-    }else{
+        document.getElementById("main").style.display = "flex"
+    } else {
         document.getElementById("nav").style.display = "grid"
-        document.getElementById("main").style.display = "flex" 
+        document.getElementById("main").style.display = "flex"
     }
 
 }
 const voltarTela = () => {
-    if(window.innerWidth < 770){
+    if (window.innerWidth < 770) {
         document.getElementById("nav").style.display = "grid"
-        document.getElementById("main").style.display = "none" 
-    }else{
+        document.getElementById("main").style.display = "none"
+    } else {
         document.getElementById("nav").style.display = "grid"
-        document.getElementById("main").style.display = "flex" 
+        document.getElementById("main").style.display = "flex"
     }
 
 }
-
-
 
 const criarMensagens = (contatoSelecionado) => {
 
@@ -87,41 +86,33 @@ const criarMensagens = (contatoSelecionado) => {
     const imageHolder = document.createElement('div')
     imageHolder.classList.add("nav__image_holder")
     const image = document.createElement('img')
-    image.setAttribute('id',"nav__header_image")
+    image.setAttribute('id', "nav__header_image")
     const name = document.createElement('h5')
     name.classList.add('contact__name', 'main__name')
     const back = document.createElement('i')
-    back.classList.add("fa-solid","fa-arrow-left","nav__icons")
-    back.setAttribute('id',"back")
-    back.addEventListener("click",voltarTela)
+    back.classList.add("fa-solid", "fa-arrow-left", "nav__icons")
+    back.setAttribute('id', "back")
+    back.addEventListener("click", voltarTela)
     let selecionadoNome = contatoSelecionado.currentTarget.querySelector('.contact__name').textContent
- 
-    
-
-    
 
     imageHolder.append(image)
     contactText.append(name)
     console.log(selecionadoNome)
 
-   
-    
-
-    contatos.forEach( (contatos) => {
-        if(contatos.name == selecionadoNome ){
+    contatos.forEach((contatos) => {
+        if (contatos.name == selecionadoNome) {
             name.textContent = selecionadoNome
-            image.setAttribute('src',"./recursos/" + contatos.image)
+            image.setAttribute('src', "./recursos/" + contatos.image)
             carregarMenssagens(contatos.messages)
         }
     })
-    contactHeader[0].replaceChildren(back,imageHolder,contactText)
-
+    contactHeader[0].replaceChildren(back, imageHolder, contactText)
 }
 
 const carregarCards = () => {
     const navMain = document.getElementById('nav__main')
-    const cards = contatos.map( criarContainer )
-   
+    const cards = contatos.map(criarContainer)
+
     navMain.replaceChildren(...cards)
 }
 
@@ -132,13 +123,12 @@ const mudarFundo = (card) => {
     containerUnselect.classList.remove('selected')
     containerSelected.classList.add('selected')
     last = card.currentTarget.id
-
-
 }
+
 const carregarMenssagens = (menssagens) => {
     const contactMain = document.getElementsByClassName("main")
     const contactMessages = menssagens.map((message) => {
-        if(message.sender == "me"){
+        if (message.sender == "me") {
             const ballon = document.createElement('div')
             ballon.classList.add("messsage__baloon__me")
             const messageContent = document.createElement('p')
@@ -146,18 +136,18 @@ const carregarMenssagens = (menssagens) => {
             messageContent.classList.add("baloon_text")
             ballon.append(messageContent)
             const time = document.createElement('span')
-            if(message.timestamp != undefined){
-                time.textContent = message.timestamp.slice(11,16)
-            }else{
+            if (message.timestamp != undefined) {
+                time.textContent = message.timestamp.slice(11, 16)
+            } else {
                 time.textContent = message.time
             }
-            
+
             time.classList.add("baloon_time")
             ballon.append(time)
-            
+
             return ballon
-            
-        }else{
+
+        } else {
             const ballon = document.createElement('div')
             ballon.classList.add("messsage__baloon")
             const messageContent = document.createElement('p')
@@ -165,14 +155,14 @@ const carregarMenssagens = (menssagens) => {
             messageContent.classList.add("baloon_text")
             ballon.append(messageContent)
             const time = document.createElement('span')
-            if(message.timestamp != undefined){
-                time.textContent = message.timestamp.slice(11,16)
-            }else{
+            if (message.timestamp != undefined) {
+                time.textContent = message.timestamp.slice(11, 16)
+            } else {
                 time.textContent = message.time
             }
             time.classList.add("baloon_time")
             ballon.append(time)
-            
+
             return ballon
 
         }
@@ -180,30 +170,23 @@ const carregarMenssagens = (menssagens) => {
     contactMain[0].replaceChildren(...contactMessages.reverse())
 }
 
-function addBallon () {
-    if(document.getElementById("send__textfield").value){
+function addBallon() {
+    if (document.getElementById("send__textfield").value) {
         const contactMain = document.getElementsByClassName("main")
         const ballon = document.createElement('div')
         ballon.classList.add("messsage__baloon__me")
         const messageContent = document.createElement('p')
-        messageContent.textContent =  document.getElementById("send__textfield").value
+        messageContent.textContent = document.getElementById("send__textfield").value
         document.getElementById("send__textfield").value = ""
         messageContent.classList.add("baloon_text")
         ballon.append(messageContent)
         const time = document.createElement('span')
         const hora = new Date()
-        time.textContent = hora.getHours() +":"+ hora.getMinutes()
-    
-                
+        time.textContent = hora.getHours() + ":" + hora.getMinutes()
         time.classList.add("baloon_time")
         ballon.append(time)
-            
-    
-        contactMain[0].insertAdjacentElement('afterBegin',ballon)
+        contactMain[0].insertAdjacentElement('afterBegin', ballon)
     }
-
 }
 
-
 carregarCards()
-console.log(contatos())

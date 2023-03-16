6bc4dd76df54ef405eff98094287cb6532de7375
Leticia Evelin <ljkeleticia@gmail.com>
2023-03-16 15:44:21 -0300

    horario adicionado

--- ds2m/letícia_evelin_aguiar_e_silva/index.js
+++ ds2m/letícia_evelin_aguiar_e_silva/index.js
@@ -22,10 +22,14 @@
     const texto = document.createElement('div')
     texto.classList.add('titulo_texto')
 
+    const horario = document.createElement('p')
+    horario.classList.add('tempo')
+    horario.textContent = contato.time
+
     const profissao = document.createElement( 'p' )
     profissao.classList.add( 'card__profissao' )
     profissao.textContent = contato.description
-   
+
     card.append(img, titulo, profissao, texto)
 
     return card
@@ -46,21 +50,25 @@
 
    let envioConversa = document.createElement('span')
 
+   let time = document.createElement('span')
+
    if(messages.sender == "me") {
       divMessages.classList.add('enviar__mensagem')
       envioConversa.classList.add('enviar__mensagem_span')
       envioConversa.textContent = messages.content
+      time.textContent = messages.time
    
     } else {
       divMessages.classList.add('mensagem__entregue')
       envioConversa.classList.add('mensagem__entregue_span')
       envioConversa.textContent = messages.content
+      time.textContent = messages.time
 
       envioConversa.textContent = messages.content
     }
    
 
-   divMessages.append(envioConversa)
+   divMessages.append(envioConversa, time)
    return divMessages
 
  }

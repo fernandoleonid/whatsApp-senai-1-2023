0dcd40c71db55b51f68407374b0c8c026ab2dfe8
FELIPE DE OLIVEIRA FLORENCIO <22282108@CFP127ALUNO.LOCAL>
2023-03-16 11:12:34 -0300

    refactor files and test 2.0

---
+++ ds2m/felipe_de_oliveira_florencio/chat-do-felipe-1-0/test/js/contact.js
@@ -0,0 +1,14 @@
+
+
+
+const buildContacts = () =>{
+    const container = document.getElementById('contact')
+    const contacts = contacts.map(createContactItem);
+    container.append(...contacts)
+}
+
+const createContactItem = function(contact){
+    
+}
+
+buildContacts()

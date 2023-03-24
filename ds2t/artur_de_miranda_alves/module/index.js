6e0b5a709e6d811d8afc72bf6a337eb404b6d3e5
ArturAlvess <contatopessoal997@gmail.com>
2023-03-24 00:28:33 -0300

    falta responsive

--- ds2t/artur_de_miranda_alves/module/index.js
+++ ds2t/artur_de_miranda_alves/module/index.js
@@ -3,7 +3,7 @@
 import {contatos} from './contatos.js'
 
 
-const cirarCard = function(contatos){
+const criarCard = function(contatos, indice){
 
     const card = document.createElement('div')
     card.classList.add("card")
@@ -20,16 +20,105 @@ const cirarCard = function(contatos){
     description.classList.add('subtitle-card')
     description.textContent = contatos.description
 
+    card.addEventListener('click', () =>{
+        let container = document.getElementById('main-right')
+        container.replaceChildren(criarHeaderMensagens(indice), criarContainerMensagens(indice), criarFooterMensagens())
+        barraDeRolagem()
+    })
+
     card.append(foto, name, description)
 
     return card
 }
 
+const criarHeaderMensagens = (indice) => {
+    const header = document.createElement('div')
+    header.classList.add('header-right')
+
+    const foto = document.createElement('img')
+    foto.classList.add('image-header-right')
+    foto.src = `./${contatos[indice].image}`
+    foto.alt = "Foto de Perfil"
+
+    const nomeDoContato = document.createElement('span')
+    nomeDoContato.classList.add('header-name')
+    nomeDoContato.textContent = contatos[indice].name
+
+    header.append(foto, nomeDoContato)
+
+    return header
+}
+
+const criarContainerMensagens = (indice) => {
+    const container = document.createElement('div')
+    container.classList.add('container-chat')
+
+    const containerMensagens = document.createElement('div')
+    containerMensagens.classList.add('container-message')
+
+    contatos[indice].messages.forEach((mensagens) => {
+
+        const caixaMensagemMinha = document.createElement('div')
+        caixaMensagemMinha.classList.add('box-message-sent')
+
+        const mensagemMinha = document.createElement('p')
+        mensagemMinha.classList.add('my-message')
+
+        const caixaMensagemSua = document.createElement('div')
+        caixaMensagemSua.classList.add('box-message-receive')
+
+        const mensagemSua = document.createElement('p')
+        mensagemSua.classList.add('your-message')
+
+        if(mensagens.sender == "me"){
+            mensagemMinha.classList.add('my-message')
+            mensagemMinha.textContent = mensagens.content
+
+            container.appendChild(containerMensagens)
+            containerMensagens.append(caixaMensagemMinha)
+            caixaMensagemMinha.append(mensagemMinha)
+
+        } else if(mensagens.sender == contatos[indice].name){
+            mensagemSua.classList.add('your-message')
+            mensagemSua.textContent = mensagens.content
+
+            container.appendChild(containerMensagens)
+            containerMensagens.append(caixaMensagemSua)
+            caixaMensagemSua.append(mensagemSua)
+        }
+
+    })
+    return container
+}
+
+const criarFooterMensagens = () => {
+    const containerMensagensFooter = document.createElement('div')
+    containerMensagensFooter.classList.add('send')
+
+    const icons = document.getElementById('emoji-anexar')
+    icons.classList.remove('emoji-anexar')
+    icons.classList.add('emoji-anexar-on')
+
+    const iconMicrofone = document.getElementById('iconMicrofone')
+    iconMicrofone.classList.remove('microfone-none')
+    iconMicrofone.classList.add('microfone')
+
+    const inputMensagem = document.createElement('input')
+    inputMensagem.placeholder = 'Mensagem'
+    inputMensagem.classList.add('send-message')
+
+    containerMensagensFooter.append(icons, inputMensagem, iconMicrofone)
+
+    return containerMensagensFooter
+}
+
 const carregarContatos = () => {
     const container_contacts = document.getElementById('left-side')
-    const card = contatos.map(cirarCard)
+    const card = contatos.map(criarCard)
 
     container_contacts.replaceChildren(...card)
 }
+
+const barraDeRolagem = () => window.scroll(0, document.body.scrollHeight)
 
 carregarContatos()

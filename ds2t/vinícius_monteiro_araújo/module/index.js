9e6ce2a95af25ecdd71d1f35add81459ded0fa88
Vinícius Monteiro <vmonteiro7843@gmail.com>
2023-03-24 13:09:20 -0300

    front finalizado

--- ds2t/vinícius_monteiro_araújo/module/index.js
+++ ds2t/vinícius_monteiro_araújo/module/index.js
@@ -4,7 +4,7 @@ import { contatos } from "./contatos.js"
 
 
 
-const criarCard = function(contatos) {
+const criarCard = function(contatos, indice) {
 
     
 
@@ -23,10 +23,110 @@ const criarCard = function(contatos) {
     description.classList.add("subtitle-card")
     description.textContent = contatos.description
 
+    card.addEventListener('click', () => {
+        let container = document.getElementById('main-right')
+        container.replaceChildren(criarHeaderMensagens(indice), criarContainerMensagens(indice),criarFooterMensagem())
+        barraDeRolagem()
+
+    })
+
+
+
    card.append(foto, name, description)
 
    
    return card
+}
+
+const criarHeaderMensagens = function(indice){
+    const header = document.createElement('div')
+    header.classList.add('header-right')
+
+    const foto = document.createElement('img')
+    foto.classList.add('header-right-image')
+    foto.src = `./${contatos[indice].image}`
+    foto.alt = 'Foto de Perfil'
+
+    const nomeContato = document.createElement('span')
+    nomeContato.classList.add('header-name')
+    nomeContato.textContent = contatos[indice].name
+
+    header.append(foto, nomeContato)
+
+    return header
+
+}
+
+const criarFooterMensagem = function(){
+    const containerMensagemFooter = document.createElement('div')
+    containerMensagemFooter.classList.add('send')
+
+    const icons = document.getElementById('emoji-anexar')
+    icons.classList.remove('emoji-anexar')
+    icons.classList.add('emoji-anexar-on')
+
+    const inputMensagem = document.createElement('input')
+    inputMensagem.placeholder = 'Mensagem'
+    inputMensagem.classList.add('send-message')
+
+    const iconMicrofone = document.getElementById('iconMicrofone')
+    iconMicrofone.classList.remove('microfone-none')
+    iconMicrofone.classList.add('microfone')
+
+    
+
+    containerMensagemFooter.append( icons, inputMensagem, iconMicrofone)
+
+    return containerMensagemFooter
+
+
+}
+
+const criarContainerMensagens = function(indice){
+    const container = document.createElement('div')
+    container.classList.add('chat-container')
+
+    const containerMensagens = document.createElement('div')
+    container.classList.add('container-chat')
+
+    contatos[indice].messages.forEach((mensagens) => {
+
+        const caixaMensagemMinha = document.createElement('div')
+        caixaMensagemMinha.classList.add('box-message-sent')
+
+        const mensagemMinha = document.createElement('p')
+        mensagemMinha.classList.add('my-message')
+
+        const caixaMensagemSua = document.createElement('div')
+        caixaMensagemSua.classList.add('box-message-receive')
+
+        const mensagemSua = document.createElement('p')
+        mensagemSua.classList.add('your-message')
+
+        if(mensagens.sender == "me"){
+            mensagemMinha.classList.add('my-message')
+            mensagemMinha.textContent = mensagens.content
+
+            container.appendChild(containerMensagens)
+            containerMensagens.append(caixaMensagemMinha)
+            caixaMensagemMinha.append(mensagemMinha)
+
+            
+        } else if (mensagens.sender == contatos[indice].name){
+            mensagemSua.classList.add('your-message')
+            mensagemSua.textContent = mensagens.content
+
+            container.appendChild(containerMensagens)
+            containerMensagens.append(caixaMensagemSua)
+            caixaMensagemSua.append(mensagemSua)
+        }
+
+
+    })
+
+    return container
+
+
 }
 
 const carregarContatos = function(){
@@ -36,7 +136,9 @@ const carregarContatos = function(){
     container_contacts.replaceChildren(...card)
 }
 
-// document.getElementById('card').addEventListener('click', mostrarConversa)
+const barraDeRolagem = function(){
+    window.scroll(0, document.body.scrollHeight)
+}
 
 
 
